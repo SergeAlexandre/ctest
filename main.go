8aefc39744db59685fc739cd57c70675b59f0904
Serge ALEXANDRE <sa@broadsoftware.com>
2020-06-23 21:37:52 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+
+type Ctest struct {
+	Name string `yaml:"name"`
+	GlobalSetup []string 	`yaml:"globalSetup"`
+	GlobalTearDown []string	`yaml:"globalTearDown"`
+	Setup []string			`yaml:"setup"`
+	TearDown[] string		`yaml:"tearDown"`
+	Tests []struct {
+		Name string `yaml:"name"`
+		Steps []struct {
+			Name string `yaml:"name"`
+			Command string `yaml:"command"`
+			RetCode *int `yaml:"retCode"`
+			Output string `yaml:"output"`
+			OutputExp string `yaml:"outputExp"`
+			Echo bool `yaml:"echo"`
+		} `yaml:"steps"`
+	} `yaml:"tests"`
+}
+
+
+func checkTest(ctest Ctest) string {
+	if ctest.Name == "" {
+		return "Missing 'name' parameter"
+	}
+	return ""
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: ctest <testfile>\n")
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Error reading '%s' as YAML file: %s\n", fileName, err)
+		os.Exit(2)
+	}
+
+	var ctest Ctest
+	err = yaml.UnmarshalStrict(yamlFile, &ctest)
+	if err != nil {
+		fmt.Printf("Error parsing YAML file: %s\n", err)
+		os.Exit(3)
+	}
+
+	mess := checkTest(ctest); if mess != "" {
+		fmt.Printf("%s in '%s' test file\n", mess, fileName)
+		os.Exit(5)
+	}
+
+	//fmt.Printf("%+v\n", ctest)
+	//tmplName := "template1.tmpl"
+	//tmpl, err := template.New(tmplName).ParseFiles(tmplName)
+	//if err != nil {
+	//	fmt.Printf("Error reading '%s' as Template file: %s\n", tmplName, err)
+	//	os.Exit(2)
+	//}
+
+	tmpl, err := template.New("template1").Parse(template1)
+	if err != nil {
+		fmt.Printf("Error loading template: %s\n", err)
+		os.Exit(4)
+	}
+	err = tmpl.Execute(os.Stdout, ctest)
+	if err != nil {
+		fmt.Printf("Error executing template: %s\n", err)
+		os.Exit(4)
+	}
+
+
+}
---
+++ template1.go
@@ -0,0 +1,80 @@
+package main
+
+
+var template1 = `
+#!/usr/bin/env bats
+
+echo "=========================== {{.Name}}"
+
+set +x
+
+failed=()
+
+appendFailed() {
+  msg="$1/$2: $3"
+  echo "FAILED: $msg"
+  failed+=("$msg")
+}
+
+
+setup() {
+    :
+{{- range .Setup}}
+    {{.}}
+{{- end}}
+}
+
+tearDown() {
+    :
+{{- range .TearDown}}
+    {{.}}
+{{- end}}
+}
+
+set -e
+# Global setup
+{{- range .GlobalSetup}}
+{{.}}
+{{- end}}
+
+set +e
+{{- range .Tests}}
+echo "--------------------------- {{.Name}}"
+TESTNAME="{{.Name}}"
+setup
+{{- range .Steps}}
+
+echo "---------- {{.Name}}"
+STEPNAME="{{.Name}}"
+output=$({{.Command}}); retCode=$?
+{{- if .Echo }}
+echo "----- $output"
+{{- end}}
+{{- if .RetCode }}
+if [ $retCode -ne {{.RetCode}} ]; then appendFailed "$TESTNAME" "$STEPNAME" "retCode is '$retCode' Should be '{{.RetCode}}'"; fi
+{{- end}}
+{{- if .Output }}
+if [ "$output" != "{{.Output}}" ]; then appendFailed "$TESTNAME" "$STEPNAME" "output is '$output' Should be '{{.Output}}'"; fi
+{{- end}}
+{{- if .OutputExp }}
+if [[ ! "$output" =~ {{.OutputExp}} ]]; then appendFailed "$TESTNAME" "$STEPNAME" "output is '$output' Should match '{{.OutputExp}}'"; fi
+{{- end}}
+{{- end}}
+tearDown
+{{- end}}
+
+
+
+# Global tearDown
+{{- range .GlobalTearDown}}
+{{.}}
+{{- end}}
+
+echo ""
+echo "${#failed[@]} failure(s)"
+
+printf '%s\n' "${failed[@]}"
+
+exit ${#failed[@]}
+
+`
